590b2e5af0214841430774cff637211e5155129a
chad-livingston <chad.t.livingston.2@gmail.com>
2021-11-14 16:40:43 -0600

    work on pt4.31

--- mooc-java-programming-i/part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
+++ mooc-java-programming-i/part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
@@ -7,7 +7,42 @@ public class SportStatistics {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Filename:");
+        String file = scan.nextLine();
+        System.out.println("Team:");
+        String userIn = scan.nextLine();
+        
+        ArrayList<Teams> teamList = readTeamsFromFile(file);
 
+        System.out.println("the list");
+        for (Teams games : teamList){
+            System.out.println(games);
+        }
+
+    }
+
+    public static ArrayList<Teams> readTeamsFromFile(String file) {
+        //ArrayList that stores team info
+        ArrayList<Teams> teams = new ArrayList<>();
+        //read the file line by line
+
+        try (Scanner readFile = new Scanner(Paths.get(file))) {
+            while (readFile.hasNextLine()) {
+                String line = readFile.nextLine();
+                String[] split = line.split(",");
+
+                String name1 = split[0];
+                String name2 = split[1];
+                int scoreT1 = Integer.valueOf(split[2]);
+                int scoreT2 = Integer.valueOf(split[3]);
+                teams.add(new Teams(name1, name2, scoreT1, scoreT2));
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        return teams;
     }
 
 }
