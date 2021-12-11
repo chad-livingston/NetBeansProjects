4cc2a94170e28cfe68c58d3e59c8acdf1ba44e37
chad-livingston <chad.t.livingston.2@gmail.com>
2021-12-11 05:12:40 -0600

    health station p05_p09

--- mooc-java-programming-i/part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ mooc-java-programming-i/part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,21 @@
 
 public class HealthStation {
 
+    public int weight;
 
     public int weigh(Person person) {
-        // return the weight of the person passed as the parameter
-        return -1;
+        // return the weight of the person passed as the parameter+
+        if (weight != 0) {
+            return weight;
+        } else {
+            this.weight = person.getWeight();
+            return weight;
+        }
+
+    }
+
+    public void feed(Person person) {
+        weight = person.getWeight() + 1;
     }
 
 }
