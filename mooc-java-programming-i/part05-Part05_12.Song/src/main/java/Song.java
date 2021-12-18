2f3f0388ca45b6ae48abc3a2e3855ce60e531f92
chad-livingston <chad.t.livingston.2@gmail.com>
2021-12-18 15:19:26 -0600

    song module completed

--- mooc-java-programming-i/part05-Part05_12.Song/src/main/java/Song.java
+++ mooc-java-programming-i/part05-Part05_12.Song/src/main/java/Song.java
@@ -10,6 +10,22 @@ public class Song {
         this.name = name;
         this.durationInSeconds = durationInSeconds;
     }
+    public boolean equals(Object compared){
+        if (this == compared){
+            return true;
+        }
+        if (!(compared instanceof Song)){
+            return false;
+        }
+        
+        Song comparedSong = (Song) compared;
+        
+        if(this.name.equals(comparedSong.name) &&
+           this.artist.equals(comparedSong.artist)){
+            return true;
+        }
+        return false;
+    }
 
     @Override
     public String toString() {
