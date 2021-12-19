2e21f094e9820e104e488ba0e0d3a058aea46a82
chad-livingston <chad.t.livingston.2@gmail.com>
2021-12-19 12:49:05 -0600

    books module completed

--- mooc-java-programming-i/part05-Part05_14.Books/src/main/java/Book.java
+++ mooc-java-programming-i/part05-Part05_14.Books/src/main/java/Book.java
@@ -12,6 +12,21 @@ public class Book {
     public String getName() {
         return name;
     }
+    public boolean equals(Object compared){
+        if (this == compared){
+            return true;
+        }
+        if (!(compared instanceof Book)){
+            return false;
+        }
+        Book comparedBook = (Book) compared;
+        
+        if (this.name.equals(comparedBook.name) && this.publicationYear == comparedBook.publicationYear){
+            return true;
+        }        
+        
+        return false;
+    }
 
     public int getPublicationYear() {
         return publicationYear;
