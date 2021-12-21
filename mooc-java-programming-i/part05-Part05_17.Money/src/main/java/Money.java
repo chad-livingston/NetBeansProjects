490934f757ee8799365fad63bda7fa985c8d7643
chad-livingston <chad.t.livingston.2@gmail.com>
2021-12-21 16:57:48 -0600

    money module completed

--- mooc-java-programming-i/part05-Part05_17.Money/src/main/java/Money.java
+++ mooc-java-programming-i/part05-Part05_17.Money/src/main/java/Money.java
@@ -23,6 +23,43 @@ public class Money {
         return this.cents;
     }
 
+    public Money plus(Money addition) {
+        Money newMoney = new Money(this.euros + addition.euros(), this.cents + addition.cents());
+        return newMoney;
+    }
+
+    public Money minus(Money decreaser) {
+
+        //Money newMoney = new Money(this.euros, this.cents);
+        int euroDiff = euros - decreaser.euros();
+        int centDiff = cents - decreaser.cents();
+        int totalDiffInCents = euroDiff * 100 + centDiff;
+        
+        if (totalDiffInCents <= 0){
+            return new Money(0,0);
+        } else {
+            int euroRtn = totalDiffInCents / 100;
+            int centRtn = totalDiffInCents % 100;
+            return new Money(euroRtn, centRtn);
+        }
+    }
+
+    public boolean lessThan(Money compared) {
+        if (this.euros < compared.euros()) {
+            return true;
+        }
+
+        if (this.euros == compared.euros()) {
+            if (this.cents < compared.cents()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        return false;
+    }
+
     public String toString() {
         String zero = "";
         if (this.cents < 10) {
