6c1bb40e59b98354b33cb11d3af9df327f38b27a
chad-livingston <chad.t.livingston.2@gmail.com>
2021-12-30 13:51:28 -0600

    intro to unit tests.

---
+++ TMCprojects/mooc-java-programming-i/exerciseManagement/src/test/java/ExerciseManagementTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Chad
+ */
+public class ExerciseManagementTest {
+    
+    public ExerciseManagementTest() {
+        
+    }
+    @Test
+    public void exerciseListEmptyAtBeginning(){
+        ExerciseManagement management = new ExerciseManagement();
+    }
+    
+}
