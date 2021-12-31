eef9ff97d8de8e6aea2a86dbae48192bf3de266c
chad-livingston <chad.t.livingston.2@gmail.com>
2021-12-30 21:50:01 -0600

    work on liquid containers module

--- TMCprojects/mooc-java-programming-i/part06-Part06_13.Exercises/src/main/java/MainProgram.java
+++ TMCprojects/mooc-java-programming-i/part06-Part06_13.Exercises/src/main/java/MainProgram.java
@@ -3,6 +3,12 @@ public class MainProgram {
 
     // update here your exercise progress
     public static int partsCompleted() {
+        ExerciseManagement management = new ExerciseManagement();
+        int partsCompleted = management.exerciseList().size();
+        if (partsCompleted == 0){
+            return 0;
+        }
+        if (partsComplete)
         return 0;
     }
 }
