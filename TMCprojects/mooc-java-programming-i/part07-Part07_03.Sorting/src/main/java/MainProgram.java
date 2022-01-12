64c6ca3aacf65cf3d45be8940778ce0d66f498b6
chad-livingston <chad.t.livingston.2@gmail.com>
2022-01-12 09:07:47 -0600

    work on sort algorithm. haven't quite figured it out yet.

--- TMCprojects/mooc-java-programming-i/part07-Part07_03.Sorting/src/main/java/MainProgram.java
+++ TMCprojects/mooc-java-programming-i/part07-Part07_03.Sorting/src/main/java/MainProgram.java
@@ -1,20 +1,13 @@
 
 import java.util.Arrays;
 
-
 public class MainProgram {
 
     public static void main(String[] args) {
         // write your test code here
-        int[] numbers = {3, 2, 5, 4, 8};
-
-        System.out.println(Arrays.toString(numbers));
-
-        MainProgram.swap(numbers, 1, 0);
-        System.out.println(Arrays.toString(numbers));
-
-        MainProgram.swap(numbers, 0, 3);
-        System.out.println(Arrays.toString(numbers));
+// indices:       0  1  2  3   4
+        int[] numbers = {8, 3, 7, 9, 1, 2, 4};
+        MainProgram.sort(numbers);
     }
 
     public static int smallest(int[] array) {
@@ -29,29 +22,23 @@ public class MainProgram {
 
     public static int indexOfSmallest(int[] array) {
         int indexOfTheSmallest = 0;
-        int counter = 0;
-        int smallest = array[0];
-        for (int num : array) {
-
-            if (num < smallest) {
-                indexOfTheSmallest = counter;
-
-            }
-            counter++;
-        }
-
-        return indexOfTheSmallest;
-    }
-
-    public static int indexOfSmallestFrom(int[] table, int startIndex) {
-        int indexOfTheSmallest = 0;
-        int smallest = table[startIndex];
-        for (int i = startIndex; i < table.length; i++) {
-            if (table[i] <= smallest) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == smallest(array)) {
                 indexOfTheSmallest = i;
             }
         }
         return indexOfTheSmallest;
+    }
+
+    public static int indexOfSmallestFrom(int[] array, int startIndex) {
+        int indexOfTheSmallestFrom = startIndex;
+        int smallest = array[startIndex];
+        for (int i = startIndex; i < array.length; i++) {
+            if (array[i] <= smallest) {
+                indexOfTheSmallestFrom = i;
+            }
+        }
+        return indexOfTheSmallestFrom;
 
     }
 
@@ -63,7 +50,23 @@ public class MainProgram {
 
         array[index1] = swapperValue;
         array[index2] = firstIndexValue;
+        
+        System.out.println("index1: " + swapperValue);
+        System.out.println("index2: " + firstIndexValue);
 
+    }
+
+    public static void sort(int[] array) {
+        System.out.println(Arrays.toString(array));
+        
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("i: " + i);
+            int smallestIndex = indexOfSmallestFrom(array, i);
+
+            swap(array, i , smallestIndex);
+
+            System.out.println(Arrays.toString(array));
+        }
     }
 
 }
