0cf221fbce4cc644d5a869ce66e7adb3661c95a4
chad-livingston <chad.t.livingston.2@gmail.com>
2022-01-12 12:08:57 -0600

    finished sort algorithm

--- TMCprojects/mooc-java-programming-i/part07-Part07_03.Sorting/src/main/java/MainProgram.java
+++ TMCprojects/mooc-java-programming-i/part07-Part07_03.Sorting/src/main/java/MainProgram.java
@@ -4,8 +4,7 @@ import java.util.Arrays;
 public class MainProgram {
 
     public static void main(String[] args) {
-        // write your test code here
-// indices:       0  1  2  3   4
+        // indices:       0  1  2  3  4  5  6
         int[] numbers = {8, 3, 7, 9, 1, 2, 4};
         MainProgram.sort(numbers);
     }
@@ -31,14 +30,15 @@ public class MainProgram {
     }
 
     public static int indexOfSmallestFrom(int[] array, int startIndex) {
-        int indexOfTheSmallestFrom = startIndex;
+        int indexOfTheSmallest = startIndex;
         int smallest = array[startIndex];
         for (int i = startIndex; i < array.length; i++) {
-            if (array[i] <= smallest) {
-                indexOfTheSmallestFrom = i;
+            if (array[i] < smallest) {
+                smallest = array[i];
+                indexOfTheSmallest = i;
             }
         }
-        return indexOfTheSmallestFrom;
+        return indexOfTheSmallest;
 
     }
 
@@ -50,20 +50,21 @@ public class MainProgram {
 
         array[index1] = swapperValue;
         array[index2] = firstIndexValue;
-        
-        System.out.println("index1: " + swapperValue);
-        System.out.println("index2: " + firstIndexValue);
+
+        //System.out.println("index1: " + swapperValue);
+        //System.out.println("index2: " + firstIndexValue);
 
     }
 
     public static void sort(int[] array) {
         System.out.println(Arrays.toString(array));
-        
-        for (int i = 0; i < array.length; i++) {
-            System.out.println("i: " + i);
-            int smallestIndex = indexOfSmallestFrom(array, i);
 
-            swap(array, i , smallestIndex);
+        for (int i = 0; i < array.length; i++) {
+
+            //System.out.println("i: " + i);
+            int smallestIndex = indexOfSmallestFrom(array, i);
+            //System.out.println("SI: " + smallestIndex);
+            swap(array, i, smallestIndex);
 
             System.out.println(Arrays.toString(array));
         }
